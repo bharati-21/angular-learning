e1ee3ac23ac9079870cc2ce791006f99dd4c1766
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-12-06 10:12:30 +0000

    refactor: change component prefix

--- hotel-inventory-app/src/app/app.component.ts
+++ hotel-inventory-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'hotel-inventory-app';
+  hotelName = 'Park Avenue';
 }
