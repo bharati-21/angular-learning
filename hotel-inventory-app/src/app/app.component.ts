4e5afe4d82c7b714c374e17462d5769497945533
Bharati Subramanian <51514137+bharati-21@users.noreply.github.com>
2023-12-06 10:15:33 +0000

    refactor: remove inline style, template

--- hotel-inventory-app/src/app/app.component.ts
+++ hotel-inventory-app/src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hinv-root',
-  template: '<h1>This is an inline template',
-  // templateUrl: './app.component.html',
-  // styleUrl: './app.component.scss',
-  styles: ['h1 { color: blueviolet; font-family: sans-serif }'],
+  // template: '<h1>This is an inline template',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  // styles: ['h1 { color: blueviolet; font-family: sans-serif }'],
 })
 export class AppComponent {
   hotelName = 'Park Avenue';
